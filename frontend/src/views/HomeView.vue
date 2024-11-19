6e432aed46560ccf657eb72a9e665457aa4abb4c
Gijs Rommers <contactnanogijs@gmail.com>
2024-11-19 10:55:06 +0100

    basic vue project with 360 coool yeey

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home">
+      <div id="panorama"></div>
+    </div>
+    <div class="cursor">Hallo allemaal</div>
+</template>
+    
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  window.pannellum.viewer('panorama', {
+    type: 'equirectangular',
+    panorama: '/images/360.png',
+    autoLoad: true
+  })
+})
+</script>
+
+  
+<style scoped>
+  #panorama {
+    width: 100%;
+    height: 500px;
+  }
+  #cursor {
+    cursor: url(/cursor.png);
+  }
+</style>
